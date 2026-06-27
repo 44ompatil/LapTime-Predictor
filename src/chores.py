c717838b58554449ec64c39bf151e624b7ebd085
Om Patil <44ompatil@gmail.com>
2026-06-27 09:41:58 +0530

    chores : script for repititive data calls

---
+++ src/chores.py
@@ -0,0 +1,72 @@
+driverInitials = [
+    'NOR',
+    'PIA',
+    'LEC',
+    'HAM',
+    'RUS',
+    'LAW',
+    'ALO',
+    'BOR',
+    'HUL',
+    'OCO',
+    'BEA',
+    'HAD',
+    'GAS',
+    'STR',
+    'CO L',
+    'TSU',
+    'ALB',
+    'VER',
+    'ANT',
+    'SAI']
+
+seasonYears = [
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,
+    2023,
+    2024,
+    2025
+    ]
+
+circuits = [
+ 'Australian Grand Prix',
+ 'Chinese Grand Prix',
+ 'Japanese Grand Prix',
+ 'Bahrain Grand Prix',
+ 'Saudi Arabian Grand Prix',
+ 'Miami Grand Prix',
+ 'Emilia Romagna Grand Prix',
+ 'Monaco Grand Prix',
+ 'Spanish Grand Prix',
+ 'Canadian Grand Prix',
+ 'Austrian Grand Prix',
+ 'British Grand Prix',
+ 'Belgian Grand Prix',
+ 'Hungarian Grand Prix',
+ 'Dutch Grand Prix',
+ 'Italian Grand Prix',
+ 'Azerbaijan Grand Prix',
+ 'Singapore Grand Prix',
+ 'United States Grand Prix',
+ 'Mexico City Grand Prix',
+ 'São Paulo Grand Prix',
+ 'Las Vegas Grand Prix',
+ 'Qatar Grand Prix',
+ 'Abu Dhabi Grand Prix']
+
+sessions = [
+	"Q",
+	"R",
+]
+
+flagsRepr = {
+	1 : "GREEN",
+	2 : "YELLOW",
+	4 : "SC",
+	5 : "RED",
+	6 : "VSCD",
+	7 : "VSCE",
+}
